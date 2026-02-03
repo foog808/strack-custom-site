d95b41708ca6cbca8afbcc51fdccb46f4dbec0f2
foog808 <129303026+foog808@users.noreply.github.com>
2026-02-03 19:43:50 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) return 'react-vendor';
+            if (id.includes('framer-motion')) return 'motion-vendor';
+            if (id.includes('@radix-ui') || id.includes('lucide-react')) return 'ui-vendor';
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
